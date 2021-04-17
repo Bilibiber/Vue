a5d5889b9a26f53f8187d8108f2bb60e48dfb6b9
Fan Guo <guofx@dukes.jmu.edu>
2021-04-17 20:59:03 +0800

    v-bind

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,13 @@
 <template>
-  <h1>Hello {{ name }}</h1>
-  <h1 v-text="name"></h1>
-  <p v-html="htmlBind"></p>
-  <button v-bind:disabled="isDisabled">Bind</button>
+  <div class="center">
+    <h1>Hello {{ name }}</h1>
+    <h1 v-text="name"></h1>
+    <p v-html="htmlBind"></p>
+    <button v-bind:disabled="isDisabled">Bind</button>
+    <p v-bind:class="className">This is a underline text</p>
+    <h2 v-bind:class="isPromoted && 'promoted'">Joker</h2>
+    <h2 v-bind:class="isSoldOut ? 'soldOut' : 'newMovie'">The Avengers</h2>
+  </div>
 </template>
 
 <script>
@@ -12,7 +17,10 @@ export default {
     return {
       name: "Mason",
       htmlBind: "<b>Hello World</b>",
-      isDisabled: true
+      isDisabled: true,
+      className: "underline",
+      isPromoted: true,
+      isSoldOut: true
     };
   }
 };
@@ -23,5 +31,24 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   top: 0;
   bottom: 0;
+}
+.underline {
+  text-decoration: underline;
+}
+.center {
+  display: grid;
+  place-items: center;
+}
+
+.promoted {
+  font-style: italic;
+}
+
+.newMovie {
+  color: greenyellow;
+}
+
+.soldOut {
+  color: red;
 }
 </style>
